1273bd2840a3d1f836cd3d519fe525122dd100ed
Nati Mask <natimask@gmail.com>
2018-02-18 00:06:20 +0200

    Get and put positionals

--- src/components/DropBox.jsx
+++ src/components/DropBox.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 require('./DropBox.less');
 
+const { mashape_key } = require('../../.env.json');
+
 const DraggableThumb = require('./DraggableThumb.jsx');
 
 module.exports = class DropBox extends React.Component {
@@ -16,6 +18,30 @@ module.exports = class DropBox extends React.Component {
         }
         this.onDragStartHandler = this.onDragStartHandler.bind(this);
         this.onDropHandler = this.onDropHandler.bind(this);
+    }
+
+    componentWillMount() {
+
+        $.ajax({
+            url: 'https://kvstore.p.mashape.com/collections/positionals/items/pos_x',
+            headers: {
+                'X-Mashape-Key': mashape_key
+            }
+        }).then(result => {
+            console.log("init pos_x:", result.value);
+            this.setState({ pos_x: parseInt(result.value) });
+        })
+
+        $.ajax({
+            url: 'https://kvstore.p.mashape.com/collections/positionals/items/pos_y',
+            headers: {
+                'X-Mashape-Key': mashape_key
+            }
+        }).then(result => {
+            console.log("init pos_y:", result.value);
+            this.setState({ pos_y: parseInt(result.value) });
+        })
+
     }
 
     onDragStartHandler(event) {
@@ -37,12 +63,39 @@ module.exports = class DropBox extends React.Component {
         event.preventDefault();
         var drop_x = event.screenX;
         var drop_y = event.screenY;
-        this.setState(prevState => ({
-            pos_x: prevState.pos_x + (drop_x - prevState.drag_begin_x),
-            pos_y: prevState.pos_y + (drop_y - prevState.drag_begin_y),
-            drag_begin_x : 0,
-            drag_begin_y : 0,
-        }))
+        var pos_x, pos_y;
+        this.setState(prevState => {
+
+            pos_x = prevState.pos_x + drop_x - prevState.drag_begin_x;
+            pos_y = prevState.pos_y + drop_y - prevState.drag_begin_y;
+
+            return {
+                pos_x, pos_y,
+                drag_begin_x : 0,
+                drag_begin_y : 0,
+            }
+
+        }, () => {
+
+            $.ajax({
+                url: 'https://kvstore.p.mashape.com/collections/positionals/items/pos_x',
+                method: 'PUT',
+                data: String(pos_x),
+                headers: {
+                    'X-Mashape-Key': mashape_key
+                }
+            })
+
+            $.ajax({
+                url: 'https://kvstore.p.mashape.com/collections/positionals/items/pos_y',
+                method: 'PUT',
+                data: String(pos_y),
+                headers: {
+                    'X-Mashape-Key': mashape_key
+                }
+            })
+
+        })
     }
 
     render() {
